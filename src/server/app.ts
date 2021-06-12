1b63de49fdd21a6b9ac2df7c859aeeb3dc52ea0c
noemicoronado <noemicoronado09@gmail.com>
2021-06-12 13:54:05 -0700

    practica6

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
-import routes from "../routes";
+// import routes from "../routes";
+import allRoutes from "../routes";
 
 class Application {
   app: express.Application;
@@ -8,10 +9,11 @@ class Application {
     this.app = express();
     this.settings();
     this.middlewares();
+    this.routes();
   }
   settings() {
     this.app.set("port", process.env.PORT || 8000);
-    this.app.use("/", routes);
+   // this.app.use("/", routes);
   }
   start() {
     this.app.listen(this.app.get("port"), () => {
@@ -22,6 +24,9 @@ class Application {
     this.app.use(morgan("dev"));
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: false }));
+  }
+  routes() {
+    this.app.use("/api", allRoutes);
   }
 }
 
