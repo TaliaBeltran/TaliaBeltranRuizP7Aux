84c12dd603c183c74a2ce67d9f403be6e57a10ab
bucles <taliabeltran.123@gmail.com>
2021-06-19 22:50:57 +0200

    PRACTICA 7

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import morgan from "morgan";
-// import routes from "../routes";
 import allRoutes from "../routes";
+import multer, { diskStorage } from "multer";
+import path from "path";
+import {v4 as uuid} from "uuid";
 
 class Application {
   app: express.Application;
@@ -10,11 +12,21 @@ class Application {
     this.settings();
     this.middlewares();
     this.routes();
+    this.static();
   }
   settings() {
     this.app.set("port", process.env.PORT || 8000);
-   // this.app.use("/", routes);
+    // this.app.use("/", routes);
+    const storage = multer.diskStorage ({
+      destination:"uploads",
+      filename: (req, file, cb ) => { 
+        cb (null, uuid() + path.extname(file.originalname));
+      },
+    });
+    this.app.use(morgan("dev"));
+    this.app.use(multer({ storage }).single("image"));
   }
+
   start() {
     this.app.listen(this.app.get("port"), () => {
       console.log(`server running on port ${this.app.get("port")}`);
@@ -27,6 +39,9 @@ class Application {
   }
   routes() {
     this.app.use("/api", allRoutes);
+  }
+  static(){
+    this.app.use("/uploads", express.static(path.resolve("uploads")));
   }
 }
 
